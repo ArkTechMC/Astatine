893fe423d7c844f7950fb223b6db73991ff5328d
IAFEnvoy <tanksheng545@126.com>
2024-10-23 14:14:25 +0800

    init

---
+++ src/main/java/com/iafenvoy/astatine/Astatine.java
@@ -0,0 +1,15 @@
+package com.iafenvoy.astatine;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Astatine implements ModInitializer {
+	public static final String MOD_ID = "astatine";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
